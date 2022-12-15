4c948f7cd657b26688ef05e24204653cec642adb
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-15 15:57:32 +0530

    First branch pushing

---
+++ GitCodeSave/Pavan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodeSave
+{
+    internal class Pavan
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Hi");
+        }
+        public static void Method1()
+        {
+            Console.WriteLine("Second");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("second");
+        }
+    }
+}
